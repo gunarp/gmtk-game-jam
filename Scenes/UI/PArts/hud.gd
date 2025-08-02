7e06f2d657d9b1e10ede24af556afa76deafbe8f
gunarp <petergunarso@outlook.com>
2025-08-02 13:21:45 -0700

    post merge cleanup

--- Scenes/UI/PArts/hud.gd
+++ Scenes/UI/PArts/hud.gd
@@ -1,13 +1,13 @@
 extends Control
 class_name HUD
 
-@export var stopwatch_lablel : Label
+@export var stopwatch_label: Label
 
-var stopwatch : Stopwatch
+var stopwatch: Stopwatch
 
 func _ready():
   stopwatch = get_tree().get_first_node_in_group("stopwatch")
-  
+
 func _process(delta):
   update_stopwatch_label()
 
