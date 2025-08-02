33d552a47a62edcd9801ceef431e55313d157d92
Sunny Hu <hu.sunnya@gmail.com>
2025-08-02 04:16:32 -0700

    whitespace changes

--- Resources/Scripts/level_collection.gd
+++ Resources/Scripts/level_collection.gd
@@ -6,8 +6,8 @@ class_name LevelCollectionResource
 var next_level_index = 0
 
 func get_current_level():
-	return level_array[next_level_index]
+  return level_array[next_level_index]
 
 func increase_level_index():
-	next_level_index += 1
-	next_level_index %= len(level_array)
+  next_level_index += 1
+  next_level_index %= len(level_array)
