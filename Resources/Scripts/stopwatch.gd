0584bb2aca0b06593377edae1edbe91a477248d7
Addison <arcothepyro@gmail.com>
2025-08-01 21:48:34 -0700

    idk.

---
+++ Resources/Scripts/stopwatch.gd
@@ -0,0 +1,14 @@
+extends Node2D
+class_name Stopwatch
+
+var time = 0.0
+var stopped = false
+
+func _process(delta):
+  if stopped:
+    return
+    time += delta
+    
+func reset():
+  time = 0.0
+  
--- Scenes/Virtual/actor.gd
+++ Scenes/Virtual/actor.gd
@@ -7,9 +7,9 @@ const TILESIZE = Vector2(70, 70)
 
 @export var speed: float = DEFAULT_SPEED
 @export_range(0, 300, 1, "suffix:px", "or_greater", "or_less", "hide_slider") var jump_height: float = 2.5:
-	set(value):
-		jump_height = value
-		jump_vel = calculate_jump_velocity(jump_height)
+  set(value):
+    jump_height = value
+    jump_vel = calculate_jump_velocity(jump_height)
 
 @onready var jump_vel = calculate_jump_velocity(jump_height)
 
@@ -25,60 +25,60 @@ signal hit_body(body)
 
 
 func calculate_jump_velocity(distance):
-	return -sqrt(2 * gravity * distance)
+  return -sqrt(2 * gravity * distance)
 
 
 func _ready():
-	pass
+  pass
 
 
 func _process(delta):
-	handle_animation(delta)
+  handle_animation(delta)
 
 
 func _physics_process(delta):
-	# The physics is split into multiple segments, to make sure each
-	# inherited scene shares properties without having to write them again and again
-	last_velocity = velocity
-	last_floor = is_on_floor()
+  # The physics is split into multiple segments, to make sure each
+  # inherited scene shares properties without having to write them again and again
+  last_velocity = velocity
+  last_floor = is_on_floor()
 
-	handle_gravity(delta)
-	handle_physics(delta)
+  handle_gravity(delta)
+  handle_physics(delta)
 
 
 func handle_physics(_delta):
-	pass
+  pass
 
 
 func handle_animation(_delta):
-	pass
+  pass
 
 
 func handle_gravity(_delta):
-	pass
-	# if not is_on_floor():
-	# 	velocity.y += gravity * delta
+  pass
+  # if not is_on_floor():
+  # 	velocity.y += gravity * delta
 
 
 func handle_cols():
-	# print(get_slide_collision_count())
-	for i in get_slide_collision_count():
-		var c = get_slide_collision(i)
-		if c.get_collider() is RigidBody2D:
-			c.get_collider().apply_central_impulse(-c.get_normal() * last_velocity.abs() * .1)
+  # print(get_slide_collision_count())
+  for i in get_slide_collision_count():
+    var c = get_slide_collision(i)
+    if c.get_collider() is RigidBody2D:
+      c.get_collider().apply_central_impulse(-c.get_normal() * last_velocity.abs() * .1)
 
 
 func _on_hitbox_body_entered(body):
-	if body != self:
-		hit_body.emit(body)
-	pass # Replace with function body.
+  if body != self:
+    hit_body.emit(body)
+  pass # Replace with function body.
 
 
 #These functions are used when one actor "hits" another actor
 func take_hit(_hitter):
-	queue_free()
-	pass
+  queue_free()
+  pass
 
 
 func react_to_hitting(_hitbody):
-	pass
+  pass
