3d5ed602f2f3a0f58a5e73d57631c54d3fc72e2c
gunarp <petergunarso@outlook.com>
2025-08-03 09:21:59 -0700

    exported again

--- Scenes/UI/Menus/level_select.gd
+++ Scenes/UI/Menus/level_select.gd
@@ -2,7 +2,7 @@ extends Control
 
 @onready var grid = %LevelGrid
 
-@onready var level_selector_scene : PackedScene = preload("res://Scenes/UI/PArts/level_selector.tscn")
+@onready var level_selector_scene: PackedScene = preload("res://Scenes/UI/Parts/level_selector.tscn")
 
 var level_index: int = 0
 
@@ -12,7 +12,8 @@ signal level_selected(level, level_id)
 func _ready():
   pass # Replace with function body.
 
-func add_level(level:LevelResource):
+
+func add_level(level: LevelResource):
   var selector := level_selector_scene.instantiate()
   grid.add_child(selector)
   selector.level_name = level.level_name
@@ -21,13 +22,16 @@ func add_level(level:LevelResource):
   selector.button.pressed.connect(signal_emit_callable)
   level_index += 1
 
-func add_all_levels(level_res:LevelCollectionResource):
+
+func add_all_levels(level_res: LevelCollectionResource):
   for level in level_res.level_array:
     add_level(level)
+
 
 func emit_level_selected(level, level_id):
   print("selected level: ", level_id)
   level_selected.emit(level, level_id)
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
